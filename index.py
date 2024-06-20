88d3f4328ec47aa270a39e11f233f2c76876c49f
Syedshara <syedsharashree@gmail.com>
2024-06-20 11:42:43 +0530

    added

---
+++ index.py
@@ -0,0 +1,9 @@
+import numpy as nu
+import torch 
+import torchvision 
+from torchvision.datasets import MNIST
+x = nu.array([[1,2],[3,4.]])
+y= torch.from_numpy(x)
+print(y)
+print(x.dtype)
+print(y.dtype);
