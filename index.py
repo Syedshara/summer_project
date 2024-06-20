489a608937c2c901ff1b356040fbf20e4950e1a4
Syedshara <syedsharashree@gmail.com>
2024-06-20 11:43:36 +0530

    sxsxs

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import torch
 import torchvision 
 from torchvision.datasets import MNIST
 x = nu.array([[1,2],[3,4.]])
-y= torch.from_numpy(x)
+y= torch.from_numpy(x);
 print(y)
 print(x.dtype)
 print(y.dtype);
